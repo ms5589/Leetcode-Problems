d250fac8ab07c102e419119ebe638da343327177
Sagar Mehta <smmehta5589@gmail.com>
2018-07-07 14:48:19 -0500

    Create Guess_Number_Higher_or_Lower.py

---
+++ Guess_Number_Higher_or_Lower.py
@@ -0,0 +1,33 @@
+''' We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example:
+n = 10, I pick 6.
+
+Return 6. '''
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 1
+        end = n
+        while(start<=end):
+            mid = (start+end)//2
+            
+            if(guess(mid)==0):
+                return mid
+            elif(guess(mid)==1):
+                start = mid + 1
+            else:
+                end = mid - 1
